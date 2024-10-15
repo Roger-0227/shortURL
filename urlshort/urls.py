72200e91c01d7bb166be049045336eefd1e205d2
roger <r1990227@hotmail.com>
2024-10-16 04:56:20 +0800

    feat add redirect to original_url

--- urlshort/urls.py
+++ urlshort/urls.py
@@ -7,5 +7,6 @@ app_name = "urlshort"
 urlpatterns = [
     path("", views.out_home, name="out_home"),
     path("index", views.index, name="index"),
+    path("<str:url>", views.redirect, name="redirect"),
     path("show/<int:id>", views.show, name="show"),
 ]
--- urlshort/views.py
+++ urlshort/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 import random
 import string
 from urlshort.models import ShortURL
@@ -10,20 +10,15 @@ def out_home(request):
 
 
 def index(request):
-    own_fields = request.GET.get("short_url")
     if request.method == "POST":
-        if own_fields == None:
-            fields = "".join(random.choices(string.ascii_letters, k=5))
-        else:
-            fields = own_fields
+        fields = radom_unique(request.POST.get("short_url"))
         form = ShortURLForm(request.POST)
-        if form.is_valid():
+        if form.is_valid() and fields:
             form = form.save(commit=False)
             form.short_url = fields
             form.save()
             return render(request, "pages/show.html", {"form": form})
-        else:
-            return render("pages/index.hmtl", {"form": form})
+        return render(request, "pages/index.html", {"form": form})
     form = ShortURLForm()
     return render(request, "pages/index.html", {"form": form})
 
@@ -31,3 +26,19 @@ def index(request):
 def show(request, id):
     url_content = get_object_or_404(ShortURL, id=id)
     return render(request, "pages/show.html", {"form": url_content})
+
+
+def redirect(request, url):
+    url_content = ShortURL.objects.get(short_url=url)
+    url = url_content.url
+    return HttpResponseRedirect(url)
+
+
+def radom_unique(str_url):
+    if not ShortURL.objects.filter(short_url=str_url).exists():
+        if str_url == None:
+            radom_field = "".join(random.choices(string.ascii_letters, k=6))
+        else:
+            radom_field = str_url
+        return radom_field
+    return False
