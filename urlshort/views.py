2be8bfbad21f9ff91c205d07dc729b3425ebc369
roger <r1990227@hotmail.com>
2024-10-18 12:46:31 +0800

    Adjust reploy path

--- urlshort/views.py
+++ urlshort/views.py
@@ -19,7 +19,7 @@ def index(request):
         form = ShortURLForm(request.POST)
         if form.is_valid() and fields:
             form = form.save(commit=False)
-            form.short_url = f"http://127.0.0.1:8000/{fields}"
+            form.short_url = f"http://54.95.125.250:8000/{fields}"
             form.is_enable = bool(is_enable)
             form.save()
             messages.success(request, "短網址完成")
@@ -37,14 +37,14 @@ def show(request, id):
 
 def redirect(request, url):
     url_content = ShortURL.objects.filter(
-        short_url=f"http://127.0.0.1:8000/{url}", is_enable=1
+        short_url=f"http://54.95.125.250:8000/{url}", is_enable=1
     ).first()
     url = url_content.url
     return HttpResponseRedirect(url)
 
 
 def random_unique(str_url):
-    short_url = f"http://127.0.0.1:8000/{str_url}"
+    short_url = f"http://54.95.125.250:8000/{str_url}"
     if not ShortURL.objects.filter(short_url=short_url).exists():
         if str_url == None:
             radom_field = "".join(random.choices(string.ascii_letters, k=6))
